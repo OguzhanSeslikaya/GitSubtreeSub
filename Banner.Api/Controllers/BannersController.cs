eec2b4b6b9ae6c08edbb8354185710c945607995
Oğuzhan Seslikaya <oguzhanseslikaya7@gmail.com>
2025-05-13 14:56:13 +0300

    deneme

---
+++ Banner.Api/Controllers/BannersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Banner.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BannersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { deneme = "asd" });
+        }
+    }
+}
