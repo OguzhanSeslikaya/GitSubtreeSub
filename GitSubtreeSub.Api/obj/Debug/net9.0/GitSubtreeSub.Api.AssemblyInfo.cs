b3f07dae7cb726018b5233a71590108e216cc12e
Oğuzhan Seslikaya <oguzhanseslikaya7@gmail.com>
2025-05-13 13:34:55 +0300

    second commit

--- GitSubtreeSub.Api/obj/Debug/net9.0/GitSubtreeSub.Api.AssemblyInfo.cs
+++ GitSubtreeSub.Api/obj/Debug/net9.0/GitSubtreeSub.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitSubtreeSub.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b61c0d9550d2b19d9498dba4bd9c87cc640a95b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitSubtreeSub.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitSubtreeSub.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
